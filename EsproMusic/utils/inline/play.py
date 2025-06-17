b1f680b920796ec7f6fef5b00f85fad4b7cbbbd3
SULTANBHAI87 <mr1233894@gmail.com>
2025-06-17 21:50:19 +0530

    Update play.py

--- EsproMusic/utils/inline/play.py
+++ EsproMusic/utils/inline/play.py
@@ -63,13 +63,18 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
        
-          [InlineKeyboardButton(text=" + ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜ ɢʀᴏᴜᴘ + ", url=f"https://t.me/MAHI_X_MUSIC_BOT?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users")],   
-        
+          
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
